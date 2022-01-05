1843dee763bac0abbfb27d8959f4018639b1c174
Lior3796 <liorlsa9@gmail.com>
2022-01-05 19:48:35 +0200

    finish project

---
+++ main.js
@@ -0,0 +1,71 @@
+const amount = document.getElementById("amount");
+const increment = document.getElementsByClassName("increment")[0];
+const decrement = document.getElementsByClassName("decrement")[0];
+const unit = document.getElementsByClassName("unit")[0];
+const box = document.getElementsByClassName("box")[0];
+const animationBubble = document.getElementsByClassName("animation-bubble")[0];
+let toggleChoice = "unit";
+
+const setCookies = () => {
+    let date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    let expires = "; expires=" + date.toGMTString();
+    document.cookie = `box=${0}` + "=" + value + expires + "; path=/";
+    document.cookie = `unit=${0}` + "=" + value + expires + "; path=/";
+}
+
+window.onload = () => {
+    const checkToggleBtn = getResultFromCookies(toggleChoice);
+    if (!checkToggleBtn) return;
+    amount.innerText = checkToggleBtn;
+}
+
+unit.addEventListener("click", () => {
+    toggleChoice = "unit";
+    amount.innerText = getResultFromCookies(toggleChoice);
+})
+box.addEventListener("click", () => {
+    toggleChoice = "box";
+    amount.innerText = getResultFromCookies(toggleChoice);
+})
+
+const countUpOrDown = (action, number) => {
+    number = parseInt(number);
+    if (action === "increment") {
+        return ++number;
+    }
+    return --number;
+}
+
+const getResultFromCookies = (toggleChoice) => {
+    const cookieValue = document.cookie
+        .split('; ')
+        ?.find(row => row.startsWith(`${toggleChoice}=`))
+        ?.split('=')[1] || undefined;
+    return cookieValue;
+}
+
+increment.addEventListener("click", () => {
+    const userResult = getResultFromCookies(toggleChoice);
+    const amountResult = countUpOrDown("increment", userResult);
+    document.cookie = `${toggleChoice}=${amountResult}`;
+    amount.innerText = amountResult;
+})
+
+decrement.addEventListener("click", () => {
+    const userResult = getResultFromCookies(toggleChoice);
+    const amountResult = countUpOrDown("decrement", userResult);
+    document.cookie = `${toggleChoice}=${amountResult}`;
+    amount.innerText = amountResult;
+})
+
+// Animation 
+unit.addEventListener("click", () => {
+    animationBubble.style.left = "0%";
+
+})
+box.addEventListener("click", () => {
+    animationBubble.style.left = "60%";
+})
+
+
---
+++ task1.md
@@ -0,0 +1,6 @@
+1. יצירת פסודו אלמנט בשם :before
+2. בקלאס arrow שימוש בwidth וheight של 70\*10
+3. לתת background blue.
+4. בarrow:before לתת content:">"
+5. לתת את אותו הwidth וheight וcolor.
+6. לעשות כנ"ל על arrow left ולהחליף בין before לafter.
